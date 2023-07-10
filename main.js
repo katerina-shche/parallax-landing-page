0fec8483dcea07ea86e8ddf3318f0d92d7cb4aa0
katerina-shche <asti.katerina@gmail.com>
2023-07-10 16:43:09 +0200

    slider js begining

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 import './style.css'
 
-
+const slideBtns = document.querySelectorAll('[data-slideBtn');
+const slideContainer = document.querySelector('[data-slideContainer]');
+const slider = [...document.querySelectorAll('[data-slide]')];
+console.log(" ~ sliderContainer", slideContainer)
+console.log(" ~ slideBtns", slideBtns)
+console.log(" ~ slider", slider)
